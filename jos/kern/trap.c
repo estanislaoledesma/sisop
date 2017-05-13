a851f3b9c4f3217ad59988850cc65f7779c368b3
martinBosch <martinbosch17@gmail.com>
2017-05-13 03:59:39 -0300

    Tarea: kern_idt casi hecha. Falta saber que va en el parametro off de SETGATE

--- jos/kern/trap.c
+++ jos/kern/trap.c
@@ -64,6 +64,24 @@ trap_init(void)
 	extern struct Segdesc gdt[];
 
 	// LAB 3: Your code here.
+	SETGATE(idt[0], 0, GD_KT, , 0);
+	SETGATE(idt[1], 0, GD_KT, , 0);
+	SETGATE(idt[2], 0, GD_KT, , 0);
+	SETGATE(idt[3], 0, GD_KT, , 0);
+	SETGATE(idt[4], 0, GD_KT, , 0);
+	SETGATE(idt[5], 0, GD_KT, , 0);
+	SETGATE(idt[6], 0, GD_KT, , 0);
+	SETGATE(idt[7], 0, GD_KT, , 0);
+	SETGATE(idt[8], 0, GD_KT, , 0);
+	SETGATE(idt[10], 0, GD_KT, , 0);
+	SETGATE(idt[11], 0, GD_KT, , 0);
+	SETGATE(idt[12], 0, GD_KT, , 0);
+	SETGATE(idt[13], 0, GD_KT, , 0);
+	SETGATE(idt[14], 0, GD_KT, , 0);
+	SETGATE(idt[16], 0, GD_KT, , 0);
+	SETGATE(idt[17], 0, GD_KT, , 0);
+	SETGATE(idt[18], 0, GD_KT, , 0);
+	SETGATE(idt[19], 0, GD_KT, , 0);
 
 	// Per-CPU setup
 	trap_init_percpu();
