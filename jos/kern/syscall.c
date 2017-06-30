ac3a1009c119ae1c2dc55eec460e63660d2e83f6
martinBosch <martinbosch17@gmail.com>
2017-06-30 20:49:18 -0300

    Arreglado bug de Triple Fault

--- jos/kern/syscall.c
+++ jos/kern/syscall.c
@@ -288,7 +288,8 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 			return sys_env_destroy(a1);
 
 		case SYS_yield:
-			return sys_yield();
+			sys_yield();
+			return 0;
 
 		default:
 			return -E_INVAL;
